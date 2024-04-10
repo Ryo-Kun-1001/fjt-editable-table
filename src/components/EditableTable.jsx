a90b62e3dada9623a04099b44621baa564532e3f
beecyan <yoppy.island4416bec@gmail.com>
2024-04-10 16:26:48 +0900

    prettier導入

--- src/components/EditableTable.jsx
+++ src/components/EditableTable.jsx
@@ -1,44 +1,31 @@
-import React from "react";
-import{
-  Table,
-  Thead,
-  Tbody,
-  Tr,
-  Th,
-  Td,
-}from '@chakra-ui/react'
+import React from 'react';
+import { Table, Thead, Tbody, Tr, Th, Td } from '@chakra-ui/react';
 function EditableTable() {
   const data = [
-    { id: 1, deadline: '4月11日', task: 'Todoアプリ完成', },
-    { id: 2, deadline: '4月12日', task: 'コードの説明会', },
+    { id: 1, deadline: '4月11日', task: 'Todoアプリ完成' },
+    { id: 2, deadline: '4月12日', task: 'コードの説明会' },
   ];
 
-  return(
+  return (
     <>
-    <Table varuant="simple">
-      <Thead>
-        <Th>ID</Th>
-        <Th>期限</Th>
-        <Th>タスク内容</Th>
-        <Th></Th>
-      </Thead>
-      <Tbody>
-        {data.map(row=>(
-        <Tr key={row.id}>
-          <Td>
-            {row.id}
-          </Td>
-          <Td>
-            {row.deadline}
-          </Td>
-          <Td>
-            {row.task}
-          </Td>
-        </Tr>
-        ))}
-      </Tbody>
-    
-    </Table>
-  </>)
+      <Table varuant="simple">
+        <Thead>
+          <Th>ID</Th>
+          <Th>期限</Th>
+          <Th>タスク内容</Th>
+          <Th></Th>
+        </Thead>
+        <Tbody>
+          {data.map(row => (
+            <Tr key={row.id}>
+              <Td>{row.id}</Td>
+              <Td>{row.deadline}</Td>
+              <Td>{row.task}</Td>
+            </Tr>
+          ))}
+        </Tbody>
+      </Table>
+    </>
+  );
 }
 export default EditableTable;
