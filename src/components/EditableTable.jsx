6ec69d362104d17b69c5f0d025539752a958a7ed
beecyan <yoppy.island4416bec@gmail.com>
2024-04-10 10:49:01 +0900

    todoアプリを作りました１

--- src/components/EditableTable.jsx
+++ src/components/EditableTable.jsx
@@ -1,4 +1,49 @@
+import React,{useState}from "react";
+import{
+  Table,
+  Thead,
+  Tbody,
+  Tr,
+  Th,
+  Td,
+  Input,
+  IconButton,
+}from '@chakra-ui/react'
 function EditableTable() {
-  return <>ここに表を作成</>;
+  const data = [
+    { id: 1, deadline: '4月11日', tasuku: 'Todoアプリ完成', },
+    { id: 2, deadline: '4月12日', tasuku: 'コードの説明会', },
+  ];
+
+  return(
+    <>
+    <Table varuant="simple">
+      <Thead>
+        <Th>ID</Th>
+        <Th>期限</Th>
+        <Th>タスク内容</Th>
+        <Th></Th>
+      </Thead>
+      <Tbody>
+        {data.map(row=>(
+        <Tr key={row.id}>
+          <Td>
+            {row.id}
+          </Td>
+          <Td>
+            {row.deadline}
+          </Td>
+          <Td>
+            {row.tasuku}
+          </Td>
+        </Tr>
+        ))}
+      </Tbody>
+    
+    </Table>
+  
+  
+  
+  </>)
 }
 export default EditableTable;
